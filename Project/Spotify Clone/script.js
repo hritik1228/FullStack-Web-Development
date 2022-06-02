920e0dacc99738bb3b14b759bc027d6a8e2fb448
hritik1228 <hritikkumar0015@gmail.com>
2022-06-02 18:35:39 +0530

    commit

---
+++ Project/Spotify Clone/script.js
@@ -0,0 +1,13 @@
+let audioElement = new Audio('songs/1.mp3');
+
+let songs = [
+        { songName: "Salam-e-Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
+        { songName: "Salam-e-Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
+        { songName: "Salam-e-Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
+        { songName: "Salam-e-Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
+        { songName: "Salam-e-Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
+        { songName: "Salam-e-Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
+        { songName: "Salam-e-Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
+        { songName: "Salam-e-Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
+    ]
+    // audioElement.play();
