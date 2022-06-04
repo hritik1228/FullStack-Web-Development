0895e2327077cb95fa30c8259896a056015dddf0
hritik1228 <hritikkumar0015@gmail.com>
2022-06-04 09:41:49 +0530

    commit

--- JavaScript/j15.js
+++ JavaScript/j15.js
@@ -1,22 +1,30 @@
 console.log("Events and Event objects");
 
+let e = document.getElementById("heading");
+document.getElementById("heading").addEventListener('click', function() {
+    console.log("hello");
+    e.innerText = 'Hello';
+
+});
+
+
 document.getElementById("idefine").addEventListener("click", function() {
 
     console.log("You have clicked the heading");
     //location.href = '//google.co.in'
 });
 
-document.getElementById("idefine").addEventListener("click", function(e) {
-    let variable;
-    variable = e.target;
-    variable = e.target.className;
-    variable = Array.from(e.target.classList);
-    //variale = e.target.id;
-    variable = e.offsetX;
-    variable = e.offsetY;
-    console.log(variable);
-    //location.href = '//google.co.in'
-});
+// document.getElementById("idefine").addEventListener("click", function(e) {
+//     let variable;
+//     variable = e.target;
+//     variable = e.target.className;
+//     variable = Array.from(e.target.classList);
+//     //variale = e.target.id;
+//     variable = e.offsetX;
+//     variable = e.offsetY;
+//     console.log(variable);
+//     //location.href = '//google.co.in'
+// });
 
 // document.getElementById("heading").addEventListener("click", function(e) {
 //     let variable = e.target.className;
--- Project/Spotify Clone/script.js
+++ Project/Spotify Clone/script.js
@@ -3,16 +3,17 @@ let songIndex = 0;
 let audioElement = new Audio('songs/1.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
 
 let songs = [
     { songName: "Salam-e-Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
-    { songName: "Salam-e-Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
-    { songName: "Salam-e-Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
-    { songName: "Salam-e-Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
-    { songName: "Salam-e-Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
-    { songName: "Salam-e-Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
-    { songName: "Salam-e-Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
-    { songName: "Salam-e-Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
+    { songName: "Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/2.jpg" },
+    { songName: "Salam-e-Ishq", filePath: "songs/3.mp3", coverPath: "covers/3.jpg" },
+    { songName: "Salam-e-Ishq", filePath: "songs/4.mp3", coverPath: "covers/4.jpg" },
+    { songName: "Salam-e-Ishq", filePath: "songs/5.mp3", coverPath: "covers/5.jpg" },
+    { songName: "Salam-e-Ishq", filePath: "songs/6.mp3", coverPath: "covers/6.jpg" },
+    { songName: "Salam-e-Ishq", filePath: "songs/7.mp3", coverPath: "covers/7.jpg" },
+    { songName: "Salam-e-Ishq", filePath: "songs/8.mp3", coverPath: "covers/8.jpg" },
 ]
 
 // audioElement.play();
@@ -23,16 +24,22 @@ masterPlay.addEventListener('click', () => {
         audioElement.play();
         masterPlay.classList.remove('fa-circle-play');
         masterPlay.classList.add('fa-circle-pause');
+        gif.style.opacity = 1;
     } else {
         audioElement.pause();
         masterPlay.classList.remove('fa-circle-pause');
         masterPlay.classList.add('fa-circle-play');
+        gif.style.opacity = 0;
     }
 })
 
 // Listen to events
-myProgressBar.addEventListener('timeupdate', () => {
-    console.log('timeupdate');
+audioElement.addEventListener('timeupdate', () => {
+    // Update SeekBar
+    progress = parseInt((audioElement.currentTime / audioElement.duration) * 100);
+    myProgressBar.value = progress;
+})
 
-    // update seekbar
+myProgressBar.addEventListener('change', () => {
+    audioElement.currentTime = myProgressBar.value * audioElement.duration / 100;
 })
