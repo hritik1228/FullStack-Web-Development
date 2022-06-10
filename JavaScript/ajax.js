a3edf03e7f9f4e9710fb165e8af780fa93d56d8c
hritik1228 <hritikkumar0015@gmail.com>
2022-06-10 11:17:12 +0530

    commit

--- JavaScript/ajax.js
+++ JavaScript/ajax.js
@@ -11,19 +11,41 @@ function buttonClickHandler() {
     const xhr = new XMLHttpRequest();
 
     // Open the object [GET request means only I will provide URL and POST request means URL + some data] true means asynchronus
-    xhr.open('GET', 'hritik.txt', true);
+    // xhr.open('GET', 'hritik.txt', true);
+    // xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos/1', true);
+    xhr.open('POST', 'https://jsonplaceholder.typicode.com/todos/1', true);
 
     // what to do OnProgress(Optional)
     xhr.onprogress = function() {
         console.log('I am on onProgress')
     }
 
+    /*
+        onreadystatechange code values
+        0 UNSENT- Client has been created.open() not called yet.
+        1 OPENED- open() has been called.
+        2 HEADERS_RECEIVED- send() has been called, and headers and status are available.
+        3 LOADING- Downloading; responseText holds partial data.
+        4 DONE- The operation is complete.
+    */
+
+    xhr.onreadystatechange = function() {
+        console.log('Ready state is', xhr.readyState);
+    }
+
     // what to do when response is ready
     xhr.onload = function() {
-        console.log(this.responseText);
+        if (this.status === 200) {
+            console.log(this.responseText);
+        } else {
+            console.log('Some error occured');
+        }
+
     }
 
     // send the request
     xhr.send();
+
+    console.log('We are completed!!');
 
 }
