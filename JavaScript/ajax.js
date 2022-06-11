ed7d71e669515d736cbac9a828e7974cb5605f08
hritik1228 <hritikkumar0015@gmail.com>
2022-06-11 11:12:10 +0530

    commit

--- JavaScript/ajax.js
+++ JavaScript/ajax.js
@@ -12,9 +12,10 @@ function buttonClickHandler() {
 
     // Open the object [GET request means only I will provide URL and POST request means URL + some data] true means asynchronus
     // xhr.open('GET', 'hritik.txt', true);
-    // xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos/1', true);
+    // xhr.open('GET', 'https://reqres.in/api/users?page=2', true);
     // use this for POST Request
-    xhr.open('POST', 'http://dummy.restapiexample.com/api/v1/employee/2', true);
+    xhr.open('POST', 'https://reqres.in/api/users', true);
+    xhr.getResponseHeader('Content-type', 'application/json');
 
     // what to do OnProgress(Optional)
     xhr.onprogress = function() {
@@ -45,8 +46,93 @@ function buttonClickHandler() {
     }
 
     // send the request
-    xhr.send();
+    params = `{
+        "name": "hritik",
+        "job": "leader"
+    }`;
+    xhr.send(params);
 
     console.log('We are completed!!');
 
+}
+
+let popBtn = document.getElementById('popBtn');
+popBtn.addEventListener('click', popHandler);
+
+function popHandler() {
+    console.log('You have clicked the pop handler');
+
+    // Instantiate an xhr object
+    const xhr = new XMLHttpRequest();
+
+    // Open the object
+    xhr.open('GET', 'http://dummy.restapiexample.com/api/v1/employees', true);
+
+
+    // What to do when response is ready
+    xhr.onload = function() {
+        if (this.status === 200) {
+            let obj = JSON.parse(this.responseText);
+            console.log(obj);
+            let list = document.getElementById('list');
+            str = "";
+            for (key in obj) {
+                str += `<li>${obj[key].employee_age} </li>`;
+            }
+            list.innerHTML = str;
+        } else {
+            console.log("Some error occured")
+        }
+    }
+
+    // send the request
+    xhr.send();
+    console.log("We are done fetching employees!");
+
+}
+
+let fetchFruit = document.getElementById('fetchFruit');
+fetchFruit.addEventListener('click', displayFruit);
+
+function displayFruit() {
+    console.log('You clicked on fetch fruits btn');
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'fruit.txt', true);
+
+    xhr.onload = function() {
+        if (this.status === 200) {
+            console.log(this.responseText);
+        } else {
+            console.log('Error occured');
+        }
+    }
+
+    xhr.send();
+
+    console.log('Task Complete');
+}
+
+let fetchVeg = document.getElementById('fetchVeg');
+fetchVeg.addEventListener('click', displayVeg);
+
+function displayVeg() {
+    console.log('You clicked on fetch Veg btn');
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'vegetable.txt', true);
+
+    xhr.onload = function() {
+        if (this.status === 200) {
+            console.log(this.responseText);
+        } else {
+            console.log('Error occured');
+        }
+    }
+
+    xhr.send();
+
+    console.log('Task Complete');
 }
