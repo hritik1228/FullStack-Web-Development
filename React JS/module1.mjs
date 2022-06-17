6bf0f703cc10c259c19af10a90ede05065bb01b6
hritik1228 <hritikkumar0015@gmail.com>
2022-06-17 12:16:44 +0530

    commit

--- React JS/module1.mjs
+++
@@ -1,9 +0,0 @@
-const a = "Hritik";
-const b = "Nikhil";
-const c = "Ankur";
-const d = "Aditya";
-
-export default a;
-export { c };
-export { b };
-export { d };
--- React JS/module2.mjs
+++
@@ -1,5 +0,0 @@
-import ui, { c, b, d } from './module1.mjs';
-console.log(ui);
-console.log(c);
-console.log(b);
-console.log(d);
