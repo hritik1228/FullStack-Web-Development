38d5089f0cc3c60d2ee1691d046942c00b95bcce
hritik1228 <hritikkumar0015@gmail.com>
2022-06-18 22:20:42 +0530

    commit

--- React JS/01-startingReact/src/App.js
+++ React JS/01-startingReact/src/App.js
@@ -2,6 +2,21 @@ import ExpenseItem from "./components/ExpenseItem";
 
 
 function App() {
+
+    const expenses=[
+        {title:'car insurance'}
+    ]
+
+
     return ( 
         <div>
-            
+            <h2> Let 's get started!</h2>
+            <ExpenseItem></ExpenseItem>
+            <ExpenseItem></ExpenseItem>
+            <ExpenseItem></ExpenseItem>
+            <ExpenseItem></ExpenseItem>
+        </div >
+    );
+}
+
+export default App;
