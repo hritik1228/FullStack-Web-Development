5bb66dbb7fd91aad99f0008582b554bc1f8eba4a
hritik1228 <hritikkumar0015@gmail.com>
2022-06-19 18:50:12 +0530

    commit

--- React JS/01-startingReact/src/components/ExpenseItem.js
+++ React JS/01-startingReact/src/components/ExpenseItem.js
@@ -1,10 +1,17 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-    
+    const Month=props.date.toLocaleString('en-US',{month:'long'});
+    const Day=props.date.toLocaleString('en-US',{day:'2-digit'});
+    const Year=props.date.getFullYear();
+
     return (
         <div className='expense-item'>
-            <div>{props.date.toISOString()}</div>
+            <div>
+                <div>{Month}</div>
+                <div>{Year}</div>
+                <div>{Day}</div>
+            </div>
             <div className='expense-item__description'>
                 <div>{props.title}</div>
                 <div className='expense-item__price'>${props.amount}</div>
