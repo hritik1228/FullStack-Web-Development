6c534df337a2957733c2fe390e54f3740a588bfe
Hritik kumar <Hritikkumar0015@gmail.com>
2022-07-09 13:32:09 +0530

    commit

---
+++ React JS/myPractice/news-monkey/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+import NavBar from './components/NavBar';
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar></NavBar>
+      </div>
+    )
+  }
+}
