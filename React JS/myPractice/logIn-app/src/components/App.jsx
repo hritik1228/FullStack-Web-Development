91f4b9b0131bc50185eb4c0ffe04d3fe636d4fc0
Hritik kumar <Hritikkumar0015@gmail.com>
2022-08-26 14:41:00 +0530

    commit

---
+++ React JS/myPractice/logIn-app/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import LogInForm from "./LogInForm";
+
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello</h1>
+      <LogInForm></LogInForm>
+    </div>
+  );
+}
+
+export default App;
