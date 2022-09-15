adfdda227333471a0509b5cda3176ca3e3b66acf
Hritik kumar <Hritikkumar0015@gmail.com>
2022-09-15 11:03:16 +0530

    commit

--- React JS/food-Order-App/src/components/Layout/HeaderCardButton.jsx
+++ React JS/food-Order-App/src/components/Layout/HeaderCardButton.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import CardIcon from "../Cart/CartIcon.jsx";
 import classes from './HeaderCardButton.module.css'
+import { useContext } from "react";
+import CartContext from "../../store/cart-context.jsx";
+
 
 const HeaderCartButton = (props) => {
+
+  const cartCtx=useContext(CartContext);
+
+  const numberOfCartItems=cartCtx.items.reduce((curNumber,item)=>{
+    return curNumber+item.amount
+  },0)
+
   return (
     <React.Fragment>
-      <button className={classes.button}>
+      <button className={classes.button} onClick={props.onClick}>
         <span className={classes.icon}>
           <CardIcon />
         </span>
         <span>Your Cart</span>
-        <span className={classes.badge}>3</span>
+        <span className={classes.badge}>{numberOfCartItems}</span>
       </button>
     </React.Fragment>
   );
