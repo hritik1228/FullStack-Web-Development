d45008311aec706534e0a9d74731ce53c5cde1a6
Hritik kumar <Hritikkumar0015@gmail.com>
2023-03-11 14:11:03 +0530

    commit

--- React JS/Projects/gpt3-website-ui_ux/src/App.jsx
+++ React JS/Projects/gpt3-website-ui_ux/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import {Footer,Blog,Possibility,Features,WhatGPT3,Header} from './containers';
+import {CTA,Brand,Navbar} from './components'
+import './App.css'
 const App = () => {
   return (
-    <div>
-      <h1>Hello React..</h1>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar/>
+        <Header/>
+      </div>
+      <Brand/>
+      <WhatGPT3/>
+      <Features/>
+      <Possibility/>
+      <CTA/>
+      <Blog/>
+      <Footer/>
     </div>
   )
 }
---
+++ React JS/Projects/gpt3-website-ui_ux/src/components/index.js
@@ -0,0 +1,5 @@
+export {default as Article} from './article/Article.jsx';
+export {default as Brand} from './brand/Brand.jsx';
+export {default as CTA} from './callToAction/CTA.jsx';
+export {default as Feature} from './feature/Feature'
+export {default as Navbar} from './navbar/Navbar'
