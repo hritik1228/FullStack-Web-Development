a3079b6b4c784044e1c13508392b4435245bb57c
Hritik kumar <Hritikkumar0015@gmail.com>
2023-03-20 13:46:20 +0530

    commit

--- React JS/Projects/gpt3-website-ui_ux/src/containers/whatGPT3/WhatGPT3.jsx
+++ React JS/Projects/gpt3-website-ui_ux/src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Features from '../features/Features'
+import Feature from '../../components/feature/Feature'
 import './WhatGPT3.css'
 const WhatGPT3 = () => {
   return (
     <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
       <div className='gpt3__whatgpt3-feature'>
-        <Features/> 
+        <Feature title='What is GPT-3' text='We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.'/> 
       </div>
 
       <div className='gpt3__whatgpt3-heading'>
@@ -14,9 +14,9 @@ const WhatGPT3 = () => {
       </div>
       
       <div className='gpt3__whatgpt3-container'>
-        <Features/>
-        <Features/>
-        <Features/>
+        <Feature title='Chatbots' text='We so opinion friends me message as delight. Whole front do of plate heard oh ought.'/>
+        <Feature title='Knowledgebase' text='At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b'/>
+        <Feature title='Education' text='At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b'/>
       </div>
 
     </div>
