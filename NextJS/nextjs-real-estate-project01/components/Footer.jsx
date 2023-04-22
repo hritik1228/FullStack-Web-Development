167fc95f38a97ae80e048ef6fcfb21162f751637
Hritik kumar <Hritikkumar0015@gmail.com>
2023-04-22 20:06:22 +0530

    commit-added header & footer

---
+++ NextJS/nextjs-real-estate-project01/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box } from '@chakra-ui/react';
+
+
+const Footer = () => {
+  return (
+    <Box textAlign="center" p="5" color="gray.600" borderTop="1px" borderColor="gray.100">
+        2023 Embassy - REITS, Inc.
+    </Box>
+  )
+}
+
+export default Footer
