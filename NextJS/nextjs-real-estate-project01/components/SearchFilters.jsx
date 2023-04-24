cb6592b9017625a6480caf31d5ddf652ea80f2d7
Hritik kumar <Hritikkumar0015@gmail.com>
2023-04-24 11:07:27 +0530

    commit-search property by filters

---
+++ NextJS/nextjs-real-estate-project01/components/SearchFilters.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useState,useEffect } from 'react';
+import {Flex,Select,Box,Text,Input,Spinner,Icon,Button} from '@chakra-ui/react';
+import {MdCancel} from 'next/image';
+import Image from 'next/image';
+import { filterData,getFilterValues } from '@/utils/filterData';
+import { useRouter } from 'next/router';
+
+const SearchFilters = () => {
+
+    const [filters,setFilters]=useState(filterData);
+    const router = useRouter();
+
+    const searchProperties=(filterValues)=>{
+        const path=router.pathname;
+        const {query} = router;
+        const values=getFilterValues(filterValues);
+        console.log(path)
+        console.log("the query is",query);
+        console.log(values)
+
+        values.forEach((item)=>{
+            // Update the query
+            query[item.name]=item.value;
+            console.log(query)
+        })
+        router.push({pathname:path,query})
+    }
+
+    return (
+        <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
+            {filters.map((filter)=>(
+                <Box key={filter.queryName}>
+                    <Select 
+                        placeholder={filter.placeholder}
+                        w="fit-content"
+                        p="2"
+                        onChange={(e)=> searchProperties({[filter.queryName]:e.target.value}) }>
+                        {filter?.items?.map((item)=>(
+                            <option value={item.value} key={item.value}>{item.name}</option>
+                        ))}
+                    </Select>
+                </Box>
+            ))}
+        </Flex>
+    )
+}
+
+export default SearchFilters
