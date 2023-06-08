323a788cc8615ecc86cb0deecfe242c8c6878ff9
Hritik kumar <Hritikkumar0015@gmail.com>
2023-06-08 10:45:11 +0530

    changed app name myapp to textutils-app

--- React JS/myPractice/form-App/src/index.js
+++ React JS/myPractice/form-App/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import UserData from "./components/UserData";
-import ContactForm from "./components/ContactForm"
+// import ContactForm from "./components/ContactForm"
 
-ReactDOM.render(<ContactForm />, document.getElementById("root"));
+ReactDOM.render(<UserData />, document.getElementById("root"));
